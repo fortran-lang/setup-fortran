281d4951e3d850a1eb3b8a7134e8f58e03b99483
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-03-14 11:55:11 +0100

    Sync fork (#8)
    * feat: set FPM environment variables (#63)
    
    * set FPM_FC/FPM_CC/FPM_CXX matching FC/CC/CXX
    * consolidate env var assignment in setup script
    
    * feat: support intel 2024.0 on linux and windows (#64)
    
    * fix(mac/gcc): always create unversioned gcc/g++/gfortran links (#65)
    
    * brew doesn't link gcc/g++ without version number like it does for gfortran
    * this was causing default mac gcc/g++ to be discovered on PATH
    * fix readme auto-update PR conditionals in reporting mode
    
    * Update compatibility matrix (#66)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * ci: don't trigger when tags are pushed (#67)
    
    * correct typo
    
    * fix variable name
    
    * pass cpp_name
    
    * pass cpp_name
    
    ---------
    
    Co-authored-by: wpbonelli <wbonelli@ucar.edu>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- install-intel-windows.bat
+++ install-intel-windows.bat
@@ -6,4 +6,4 @@ start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted --log %TEM
 del %TEMP%\webimage.exe
 
 :: run installer
-%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=intel.oneapi.win.cpp-compiler:intel.oneapi.win.ifort-compiler --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=%TEMP%
+%TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=%2 --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=%TEMP%
