8bc199e929678e8fb4ae289153cf039e874a5655
minhqdao <hello@minhdao.de>
2026-08-28 05:00:37 +0700

    Remove never supported version

--- src/installers/ifort/win32.ts
+++ src/installers/ifort/win32.ts
@@ -44,10 +44,6 @@ const IFORT_RELEASES = [
     url: "https://registrationcenter-download.intel.com/akdlm/IRC_NAS/2a13d966-fcc5-4a66-9fcc-50603820e0c9/w_HPCKit_p_2023.1.0.46357_offline.exe",
   },
   {
-    version: "2021.8",
-    url: "https://registrationcenter-download.intel.com/akdlm/irc_nas/19086/m_HPCKit_p_2023.0.0.25440_offline.exe",
-  },
-  {
     version: "2021.7",
     url: "https://registrationcenter-download.intel.com/akdlm/irc_nas/18857/w_HPCKit_p_2022.3.0.9564_offline.exe",
   },
